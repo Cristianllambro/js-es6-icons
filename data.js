e68d045674f414934472097a2f7cfbdac407d169
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-09 17:57:28 +0100

    asd

--- data.js
+++ data.js
@@ -114,20 +114,25 @@ const arrIcon =
 	}
 ];
 
-const container = document.getElementById('container');
 
 //generate card
-arrIcon.forEach( (el) => {
-	generateIcons(el.family, el.prefix, el.name, el.color)
-});
 
 
 //function for card
-function generateIcons (family, prefix, name, color) {
-	let allCard = document.createElement('div');
-	allCard.classList.add('card');
-	allCard.innerHTML = `<i class="${family} ${prefix}${name}" style="color: ${color}"></i><p class="text-icon">${name}</p>`
-	container.append(allCard);
+const container = document.getElementById('container');
+
+function generateIcon () {
+	
+	arrIcon.forEach( (el) => {
+		
+		let allCard = document.createElement('div');
+		allCard.classList.add('card');
+		allCard.innerHTML = `<i class="${el.family} ${el.prefix}${el.name}" style="color: ${el.color}"></i>
+		<p class="text-icon">${el.name}</p>`
+		container.append(allCard);
+	});
+	
+
 };
 
 // function for filter
